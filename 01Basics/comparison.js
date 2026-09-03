46544f7267ef3cea764eaebeca96fcfc40ef3b02
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-03 22:16:56 +0530

    coparison check

---
+++ 01Basics/comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 1);
+console.log("02">1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)   //it will print true because >= first convert null to zero 
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
