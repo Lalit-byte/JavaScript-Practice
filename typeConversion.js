136f825915caca531e1a8eaf7f7f50fea749a79b
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-03 21:52:16 +0530

    type conversion

---
+++ typeConversion.js
@@ -0,0 +1,26 @@
+let  score = "33"
+
+console.log(typeof score)
+
+let value = Number(score)
+console.log(value)
+console.log(typeof value)
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false=0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => 0
+// "Lalit" => 1
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
