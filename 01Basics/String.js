68a1dc491bf942f0eb86fffb81bbfdd2100b4ca4
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-04 10:52:30 +0530

    practiced Strings

---
+++ 01Basics/String.js
@@ -0,0 +1,17 @@
+let name = "Lalit"
+
+console.log(`My name is ${name}. I have done MCA.`)    
+
+//to make array from string-->>>>
+
+let gameName = "Pg-Thakur-818"
+
+console.log(gameName.split("-"))   //[ 'Pg', 'Thakur', '818' ]
+
+
+// replace function
+let  a = "  hello this side lalit    "
+console.log(a.replace("side","is"))
+
+//remove trailing and ending spaces-->>
+console.log(a.trim())
