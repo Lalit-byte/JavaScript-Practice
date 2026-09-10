7b5366e7742bef0e352c4aacde676bbbd90e5ddb
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-10 22:49:06 +0530

    promisses learning

--- 09_APT/Promises.js
+++ 09_APT/Promises.js
@@ -93,16 +93,17 @@ consumerPromiseFive()
 
  
 // fetching data 
-async function getAllUser(){
-    try {
-       const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        console.log(data) 
-    } catch (error) {
-       console.log("E: ",error) 
-    }
-}
-getAllUser()
+// async function getAllUser(){
+//     try {
+//        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data) 
+//     } catch (error) {
+//        console.log("E: ",error) 
+//     }
+// }
+// getAllUser()
+
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -112,6 +113,5 @@ fetch('https://jsonplaceholder.typicode.com/users')
     console.log(data)
 })
 .catch((err)=> console.log(error))
-
 
 
