3694bba6461ecc34dff20ab6a8e6f5cb4ec8e8bc
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-16 23:57:00 +0530

    learning oops

--- 10_OOP/oop.js
+++ 10_OOP/oop.js
@@ -12,16 +12,24 @@
 
 // console.log(user.getUserDetails())
 
-
 // Using constructor
 
-function User(username, loginCount , email){
-    this.username = username;
-    this.loginCount = loginCount;
-    this.email = email;
+function User(username, loginCount, email) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.email = email;
 
-    return this;
+  return this;
 }
 
-const userOne = new User("Lalit","6","lalit@mail.com")
-console.log(userOne.username)
+// if we don't use new keyword and try to make another user(userTwo) then userTwo will override the value of userOne
+const userOne = new User("Lalit", "6", "lalit@mail.com");
+console.log(userOne.constructor);
+
+const userTwo = new User("Thakur", "15" , "thakur@mail.com")
+// console.log(userTwo.username)
+
+//step1 new keyword creates a empty object calls instance
+//step2 new keyword calls constructor function
+//step3 all the passed data will inject to this
+//step 4 we get all data inside function
